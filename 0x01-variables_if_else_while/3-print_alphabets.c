1f82f3827f4d42bf851019c062b99c7bed22d7c0
David <braveukay1@gmail.com>
2023-10-07 07:12:45 -0700

    Display alphabet in lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int j= 97;
+	int k= 65;
+
+	while (j <= 122)
+	{
+		putchar(j);
+		j++;
+	}
+	while (k <= 90)
+	{
+		putchar(k);
+		k++;
+	}
+		putchar('\n');
+		return (0);
+}
