e27c08dca52e29370e3d6059139918cfd3058838
David <braveukay1@gmail.com>
2023-10-10 14:44:50 -0700

    Display alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *print_alphabet - Display alphabet in lowercase
+  *Return: void
+*/
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+	return (void);
+}
