c6ac425c17c44a1258b37d1f2036e24cfbdc8405
David <braveukay1@gmail.com>
2023-10-10 18:57:53 -0700

    Display the sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * add - adds two numbers and returns the result
+  * @a: first number to be added
+  * @b: second number to be added
+  * Return: sum of a and b
+*/
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
