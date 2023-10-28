e7d798f7f76e7bbcf070f8db00d683a525bebdcd
David <braveukay1@gmail.com>
2023-10-27 21:28:52 -0700

    Hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 /**
  * main - add positive numbers
  * @argc: argument count
@@ -12,17 +11,22 @@ int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
+	if (arg < 2)
+	{
+		printf("0\n")
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		sum += atoi(argv[i][j]);
+		sum = sum + atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
