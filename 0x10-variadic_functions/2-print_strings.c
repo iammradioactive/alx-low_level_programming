e015cc312f1eaee141566c9ec2cc49742dfb846e
David <braveukay1@gmail.com>
2023-11-10 00:55:43 -0800

    write function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(list, char *);
 		if (!str)
-			str = "nil";
+			str = "(nil)";
 		if (!separator)
 			printf("%s", str);
 		else if (separator && i == 0)
@@ -26,6 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s%s", separator, str);
 	}
+
 	printf("\n");
 	va_end(list);
 }
