6170a0c37cfb6121af2a73414ac3ab5535d31851
David <braveukay1@gmail.com>
2023-11-16 14:32:28 -0800

    print string before main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * pre_main - prints string before main is executed
+  */
+
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
