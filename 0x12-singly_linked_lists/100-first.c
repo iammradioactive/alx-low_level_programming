5e7aed60b11225042e3750234bac30d9fc53d221
David <braveukay1@gmail.com>
2023-11-16 14:45:15 -0800

    write a function that frees a list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
   * Return: no return
   */
 
-void pre_main(void)
+void __attribute__ ((constructor)) pre_main()
 {
 	printf("You're beat! and yet, you must allow, \n");
 	printf("I bore my house upon my back!\n");
